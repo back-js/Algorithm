8fe58d9d2d94a9027b3f5dd554af65bc874b046c
bask-js <back@yonsei.ac.kr>
2021-01-01 17:27:40 +0900

    'practice'

--- Level2/skill_tree.py
+++ Level2/skill_tree.py
@@ -30,3 +30,22 @@ BDA: B 스킬을 배우기 전에 C 스킬을 먼저 배워야 합니다. 불가
 
 '''
 
+
+def solution(skill, skill_trees):
+    answer = 0
+    skill = list(skill)
+    flag = []
+    for i in skill_trees:
+        # 가능한 스킬인지 검증
+        post_skill_trees = []
+        for j in list(i):
+            if j in skill:
+                post_skill_trees.append(j)
+
+        for idx, j in enumerate(post_skill_trees):
+            if j != skill[idx]:
+                flag.append(-1)
+                break
+    return len(skill_trees) + sum(flag)
+
+solution('CBD',["BACDE", "CBADF", "AECB", "BDA"])
