cf24a7e3058bb54e8cb26090af2098e96eb623ea
bask-js <back@yonsei.ac.kr>
2021-01-26 14:17:10 +0900

    '0126'

---
+++ baekjun/2178.py
@@ -0,0 +1,112 @@
+'''
+
+미로에서 1은 이동할 수 있는 칸을 나타내고, 0은 이동할 수 없는 칸을 나타낸다. 이러한 미로가 주어졌을 때,
+(1, 1)에서 출발하여 (N, M)의 위치로 이동할 때 지나야 하는 최소의 칸 수를 구하는 프로그램을 작성하시오.
+한 칸에서 다른 칸으로 이동할 때, 서로 인접한 칸으로만 이동할 수 있다.
+
+위의 예에서는 15칸을 지나야 (N, M)의 위치로 이동할 수 있다. 칸을 셀 때에는 시작 위치와 도착 위치도 포함한다.
+
+입력
+첫째 줄에 두 정수 N, M(2 ≤ N, M ≤ 100)이 주어진다. 다음 N개의 줄에는 M개의 정수로 미로가 주어진다. 각각의 수들은 붙어서 입력으로 주어진다.
+
+출력
+첫째 줄에 지나야 하는 최소의 칸 수를 출력한다. 항상 도착위치로 이동할 수 있는 경우만 입력으로 주어진다.
+
+예제 입력 1
+4 6
+101111
+101010
+101011
+111011
+예제 출력 1
+15
+예제 입력 2
+4 6
+110110
+110110
+111111
+111101
+예제 출력 2
+9
+예제 입력 3
+2 25
+1011101110111011101110111
+1110111011101110111011101
+예제 출력 3
+38
+예제 입력 4
+7 7
+1011111
+1110001
+1000001
+1000001
+1000001
+1000001
+1111111
+예제 출력 4
+13
+
+
+'''
+N,M=map(int,input().split())
+matrix = []
+visited = [[0]*M for _ in range(N)]
+
+for i in range(N) :
+    s = list(map(int,input().split('')))
+    matrix.append(s)
+
+dx,dy = [-1,1,0,0],[0,0,-1,1]
+
+queue = [(0,0)]
+visited[0][0] = 1
+
+while True:
+    x,y = queue.pop(0)
+    if x == N -1 and y == M-1 :
+        print(visited[x][y])
+        break
+
+    for i in range(4):
+        nx,ny = x+dx[i] , y+dy[i]
+        if 0 < nx <= N and 0 < ny <= M :
+            if visited[nx][ny] == 0 and matrix[nx][ny] == 1 :
+                visited[nx][ny] = visited[x][y] + 1
+                queue.append(nx,ny)
+
+
+
+####
+
+from sys import stdin
+N, M = map(int, stdin.readline().split())
+# matrix 배열
+matrix = [stdin.readline().rstrip() for _ in range(N)]
+# 방문경로 배열
+visited = [[0]*M for _ in range(N)]
+# 좌/우/위/아래 방향 이동
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+
+# BFS 경로 탐색
+# queue 방식 사용
+queue = [(0,0)]
+visited[0][0] = 1
+
+while queue:
+    x, y = queue.pop(0)
+
+    if x == N-1 and y == M-1:
+        # 최종 경로 도착
+        print(visited[x][y])
+        break
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < N and 0 <= ny < M:
+            if visited[nx][ny] == 0 and matrix[nx][ny] == '1':
+                visited[nx][ny] = visited[x][y] + 1
+                queue.append((nx,ny))
+
+
+
--- test.py
+++ test.py
@@ -1,5 +1,26 @@
-s = [1,2,3]
+N,M=map(int,input().split())
+matrix = []
+visited = [[0]*M for _ in range(N)]
 
-k = s.pop()
-print(s)
-print(k)
+for i in range(N) :
+    s = list(map(int,str(input())))
+    matrix.append(s)
+
+dx,dy = [-1,1,0,0],[0,0,-1,1]
+
+queue = [(0,0)]
+visited[0][0] = 1
+while True:
+    x,y = queue.pop(0)
+
+    if x == N -1 and y == M-1 :
+        print(visited[x][y])
+        break
+
+    for i in range(4):
+        nx = x+dx[i]
+        ny = y+dy[i]
+        if 0 <= nx < N and 0 <= ny < M :
+            if visited[nx][ny] == 0 and matrix[nx][ny] == 1 :
+                visited[nx][ny] = visited[x][y] + 1
+                queue.append((nx,ny))
