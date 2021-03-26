d9d92433b0413290932e3431d7a65314dd16e502
backjs <back@yonsei.ac.kr>
2021-03-26 23:36:14 +0900

    thanks

--- baekjun/15650.py
+++ baekjun/15650.py
@@ -1,15 +1,15 @@
 '''
 
-문제
 자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
 
 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+고른 수열은 오름차순이어야 한다.
 입력
 첫째 줄에 자연수 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 8)
 
 출력
 한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 중복되는 수열을 여러 번 출력하면 안되며, 
- 수열은 공백으로 구분해서 출력해야 한다.
+각 수열은 공백으로 구분해서 출력해야 한다.
 
 수열은 사전 순으로 증가하는 순서로 출력해야 한다.
 
@@ -25,75 +25,16 @@
 1 2
 1 3
 1 4
-2 1
 2 3
 2 4
-3 1
-3 2
 3 4
-4 1
-4 2
-4 3
 예제 입력 3 
 4 4
 예제 출력 3 
 1 2 3 4
-1 2 4 3
-1 3 2 4
-1 3 4 2
-1 4 2 3
-1 4 3 2
-2 1 3 4
-2 1 4 3
-2 3 1 4
-2 3 4 1
-2 4 1 3
-2 4 3 1
-3 1 2 4
-3 1 4 2
-3 2 1 4
-3 2 4 1
-3 4 1 2
-3 4 2 1
-4 1 2 3
-4 1 3 2
-4 2 1 3
-4 2 3 1
-4 3 1 2
-4 3 2 1
 
 '''
 
-
-
-N,M = map(int,input().split())
-visited = [False] * N
-out = []
-
-def solve(depth,N,M) :
-    if depth == M :
-        print(' '.join(map(str,out)))
-        return
-    for i in range(N) :
-        if visited[i] == False :
-            visited[i] = True
-            out.append(i+1)
-            solve(depth+1,N,M)
-            visited[i] = False
-            out.pop()
-        
-
-
-
-
-
-
-
-
-
-
-
-######
 N, M = map(int, input().split())
 visited = [False] * N  # 탐사 여부 check
 out = []  # 출력 내용
@@ -102,7 +43,7 @@ def solve(depth, N, M):
     if depth == M:  # 탈출 조건
         print(' '.join(map(str, out)))  # list를 str으로 합쳐 출력
         return
-    for i in range(len(visited)):  # 탐사 check 하면서
+    for i in range(depth,len(visited)):  # 탐사 check 하면서
         if not visited[i]:  # 탐사 안했다면
             visited[i] = True  # 탐사 시작(중복 제거)
             out.append(i+1)  # 탐사 내용
