c2765813e9b4e4b77f0fb13a089fd00a655cf4e0
backjs <back@yonsei.ac.kr>
2021-04-05 08:57:47 +0900

    0405

--- test2.py
+++ test2.py
@@ -1,26 +1,3 @@
-import os
-import json
-import requests
-
-def sendToMeMessage(text):
-    header = {"Authorization": 'Bearer ' + KAKAO_TOKEN}
-
-    url = "https://kapi.kakao.com/v2/api/talk/memo/default/send" #나에게 보내기 주소
-
-    post = {
-        "object_type": "text",
-        "text": text,
-        "link": {
-            "web_url": "https://developers.kakao.com",
-            "mobile_web_url": "https://developers.kakao.com"
-        },
-        "button_title": "바로 확인"
-    }
-    data = {"template_object": json.dumps(post)}
-    return requests.post(url, headers=header, data=data)
-
-text = "Hello, This is KaKao Message Test!!("+os.path.basename(__file__).replace(".py", ")")
-
-KAKAO_TOKEN = "0BBD6MQH69cXbiduLDdQms-m9mSXG1jAewXxiAo9cxcAAAF4PeDZXA"
-
-print(sendToMeMessage(text).text)
+n =10
+for i in range(n,0,-1) :
+    print(i)
