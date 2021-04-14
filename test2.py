c903db5d271991d817e8bf6f5fbab7aac3d4698e
backjs <back@yonsei.ac.kr>
2021-04-14 22:10:09 +0900

    re challenge!

--- test2.py
+++ test2.py
@@ -1,7 +1,13 @@
-import sys
+import requests
 
+url = "https://kakaoi-newtone-openapi.kakao.com/v1/synthesize"
 
-s=  sys.stdin.readline().rstrip()[1:-1].split(',')
-ss = list(map(int, s))
-print(type(ss))
-print(ss)
+payload="<speak> 그는 그렇게 말했습니다. </speak>"
+headers = {
+  'Content-Type': 'application/xml',
+  'Authorization': 'KakaoAK 264fb41b81e56fae1b785fafb9111a9b'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
