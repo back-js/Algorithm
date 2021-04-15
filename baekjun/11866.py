4bbac5f9f7828c0a70e71ce2ad358cf5d01af4db
backjs <back@yonsei.ac.kr>
2021-04-15 13:12:20 +0900

    dd

--- baekjun/11866.py
+++ baekjun/11866.py
@@ -1,1 +1,11 @@
-n
+N,K = map(int,input().split())
+
+cycle = [i for i in range(1,N+1)]
+result = []
+while cycle :
+    for i in range(K-1):
+        a = cycle.pop(0)
+        cycle.append(a)
+    value  = cycle.pop(0)
+    result.append(value)
+print('<'+', '.join(map(str, result))+'>')
